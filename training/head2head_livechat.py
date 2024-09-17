246c690a44e354b51c20d4d5f3b5dc18208b2d13
navidm <navidm@zillowgroup.com>
2024-09-17 12:50:52 -0400

    livechat script updated

--- training/head2head_livechat.py
+++ training/head2head_livechat.py
@@ -3,15 +3,31 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig, LlamaF
 from fire import Fire
 from typing import List, Tuple
 import torch
+import json
+from prompting.llama_prompt import modified_extes_support_strategies
+
+
+
+class SysMsg:
+    def __init__(self):
+        self.cur_strategy = None
+
+    def __str__(self):
+        if self.cur_strategy is None:
+            return ""
+
+        description = modified_extes_support_strategies[self.cur_strategy]
+        return """You are a helpful and caring AI which is an expert in emotional support.\
+ A user has come to you for help. Use "{cur_strategy}" strategy ({strategy_description}) for answering the user.\
+ make your response short and to the point.""".format(cur_strategy=self.cur_strategy,
+                                                      strategy_description=description)
 
 
 # todo: add streamer to generation
 def run(model_name_or_path_1='Qwen/Qwen1.5-0.5B-Chat', model_name_or_path_2='meta.llama3-8b-instruct-v1:0',
         max_new_tokens=512, cache_dir=None,
         temperature=0.7, top_p=0.9, num_return_sequences=1, do_sample=True, num_beams=1, share=False):
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 
     model_1 = AutoModelForCausalLM.from_pretrained(model_name_or_path_1, cache_dir=cache_dir)
     model_1 = model_1.to(device)
@@ -23,13 +39,31 @@ def run(model_name_or_path_1='Qwen/Qwen1.5-0.5B-Chat', model_name_or_path_2='met
     model_2.eval()
     tokenizer_2 = AutoTokenizer.from_pretrained(model_name_or_path_2, cache_dir=cache_dir)
 
+    # Keep track of the selected strategy
+    strategies = list(modified_extes_support_strategies.keys())
+    cur_sys_msg = SysMsg()
+
+    def set_strategy(strategy_str: str):
+        """ Update the selected strategy based on user input """
+        cur_sys_msg.cur_strategy = strategy_str
+
+        print(f"Strategy set to: {cur_sys_msg.cur_strategy}")
+        return f"Strategy set to: {cur_sys_msg.cur_strategy}"
+
+    def clear_chat_and_reset_strategy():
+        """ Clear chat and reset the strategy """
+        cur_sys_msg.cur_strategy = None
+        print("Chat and strategy cleared")
+        return [], [], "No strategy selected"
 
     def build_input_from_chat_history(chat_history: List[Tuple], msg: str):
-        messages = [{'role': 'system', 'content': ""}]
+        """ Build the input by combining the chat history with the system message. """
+        messages = [{'role': 'system', 'content': str(cur_sys_msg)}]
         for user_msg, ai_msg in chat_history:
             messages.append({'role': 'user', 'content': user_msg})
             messages.append({'role': 'assistant', 'content': ai_msg})
         messages.append({'role': 'user', 'content': msg})
+        print(messages)
         return messages
 
     # Define the chat function for model 1
@@ -43,17 +77,30 @@ def run(model_name_or_path_1='Qwen/Qwen1.5-0.5B-Chat', model_name_or_path_2='met
         )
         input_ids = input_ids.to(device)
         output = model.generate(input_ids, max_new_tokens=max_new_tokens, pad_token_id=tokenizer.eos_token_id,
-                                  do_sample=do_sample, temperature=temperature, top_p=top_p,
-                                  num_return_sequences=num_return_sequences, num_beams=num_beams)
+                                do_sample=do_sample, temperature=temperature, top_p=top_p,
+                                num_return_sequences=num_return_sequences, num_beams=num_beams)
         response_token_ids = output[0][input_ids.shape[1]:]
         response = tokenizer.decode(response_token_ids, skip_special_tokens=True)
         chat_history.append((message, response))
         return "", chat_history
 
+    def m1_fn(msg, chat_history):
+        return chat_model(model_1, tokenizer_1, msg, chat_history)
+
+    def m2_fn(msg, chat_history):
+        return chat_model(model_2, tokenizer_2, msg, chat_history)
 
     iface = gr.Blocks()
 
     with iface:
+        gr.Markdown("### Choose a Strategy")
+        strategy_textbox = gr.Textbox(value="No strategy selected", label="Current Strategy", interactive=False)
+        with gr.Row():
+            for strategy in strategies:
+                s = gr.Button(strategy, elem_id=f"strategy_{strategy}")
+                s.click(lambda s=strategy: set_strategy(s), None, strategy_textbox)
+
+
         gr.Markdown("# Compare Two Models Side by Side")
         with gr.Row():
             with gr.Column():
@@ -66,11 +113,11 @@ def run(model_name_or_path_1='Qwen/Qwen1.5-0.5B-Chat', model_name_or_path_2='met
 
         with gr.Row():
             msg = gr.Textbox(label="Your message here")
-            msg.submit(chat_model, [model_1, tokenizer_1, msg, chatbox_1], [msg, chatbox_1])
-            msg.submit(chat_model, [model_2, tokenizer_2, msg, chatbox_2], [msg, chatbox_2])
-            clear = gr.Button("Clear Chat")
-            clear.click(lambda: None, None, chatbox_1, queue=False)
-            clear.click(lambda: None, None, chatbox_2, queue=False)
+            msg.submit(m1_fn, [msg, chatbox_1], [msg, chatbox_1])
+            msg.submit(m2_fn, [msg, chatbox_2], [msg, chatbox_2])
+            clear = gr.Button("Clear Chat and Strategy")
+            clear.click(clear_chat_and_reset_strategy, None, [chatbox_1, chatbox_2, strategy_textbox], queue=False)
+
 
     # Launch the app
     iface.launch(share=share)
--- training/lora_finetuning_llama.sh
+++ training/lora_finetuning_llama.sh
@@ -1,34 +1,34 @@
-#
-#WANDB_MODE=online WANDB_ENTITY=navidmdn WANDB_PROJECT=esconv-sra-lora python lora_finetuning_llama.py\
-#  --train_file data/train_processed.json\
-#  --dev_dir data/\
-#  --output_dir outputs/llama2_lora_r128a256\
-#  --do_train\
-#  --do_eval\
-#  --model_id meta-llama/Llama-2-7b-chat-hf\
-#  --use_peft\
-#  --cache_dir ../../hfcache\
-#  --include_inputs_for_metrics\
-#  --per_device_train_batch_size 2\
-#  --per_device_eval_batch_size 4\
-#  --gradient_accumulation_steps 32\
-#  --num_train_epochs 5\
-#  --save_strategy steps\
-#  --eval_strategy steps\
-#  --save_total_limit 1\
-#  --metric_for_best_model "main_val_loss"\
-#  --save_steps 20\
-#  --logging_steps 1\
-#  --overwrite_output_dir\
-#  --report_to wandb\
-#  --load_best_model_at_end\
-#  --eval_steps 20\
-#  --max_new_tokens 1024\
-#  --warmup_ratio 0.1\
-#  --lr_scheduler_type "cosine_with_restarts"\
-#  --lr_scheduler_kwargs '{"num_cycles": 5}'\
-#  --lora_r 128\
-#  --lora_a 256
+
+WANDB_MODE=online WANDB_ENTITY=navidmdn WANDB_PROJECT=esconv-sra-lora python lora_finetuning_llama.py\
+  --train_file data/train_processed.json\
+  --dev_dir data/\
+  --output_dir outputs/llama2_lora_r128a256\
+  --do_train\
+  --do_eval\
+  --model_id meta-llama/Llama-2-7b-chat-hf\
+  --use_peft\
+  --cache_dir ../../hfcache\
+  --include_inputs_for_metrics\
+  --per_device_train_batch_size 2\
+  --per_device_eval_batch_size 4\
+  --gradient_accumulation_steps 32\
+  --num_train_epochs 5\
+  --save_strategy steps\
+  --eval_strategy steps\
+  --save_total_limit 1\
+  --metric_for_best_model "main_val_loss"\
+  --save_steps 20\
+  --logging_steps 1\
+  --overwrite_output_dir\
+  --report_to wandb\
+  --load_best_model_at_end\
+  --eval_steps 20\
+  --max_new_tokens 1024\
+  --warmup_ratio 0.1\
+  --lr_scheduler_type "cosine_with_restarts"\
+  --lr_scheduler_kwargs '{"num_cycles": 5}'\
+  --lora_r 128\
+  --lora_a 256
 
 
 WANDB_MODE=online WANDB_ENTITY=navidmdn WANDB_PROJECT=esconv-sra-lora python lora_finetuning_llama.py\
